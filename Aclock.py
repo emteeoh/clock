5635f9a1cade6c7e3c8e6a852787478f443ab12f
MTO <emteeoh@gmail.com>
2018-04-23 22:24:08 -0400

    And some more minor cleanup.

--- Aclock.py
+++ Aclock.py
@@ -2,13 +2,12 @@ import time
 import board
 import neopixel
 from fontdef import *
+import gc
 
 
 def foo():
-    colour = 0x3333ff
     tf = '{:02}:{:02}:{:02}'
     hours = minutes = seconds = 0
-
     def drawtime(t, p, c):
         p.fill((0, 0, 0,))
         offset = 0
@@ -18,12 +17,10 @@ def foo():
             offset += len(font[fontmap.index(digit)])
         p.show()
 
-    pixels = neopixel.NeoPixel(board.D4, 256, bpp=3, brightness=0.3, auto_write=False)
-    pixels.fill((0, 0, 0))
-    pixels.show()
+    pixels = neopixel.NeoPixel(board.D4, 256, bpp=3, brightness=0.1, auto_write=False)
     while True:
         currenttime = tf.format(hours, minutes, seconds)
-        drawtime(currenttime, pixels, colour)
+        drawtime(currenttime, pixels, 0x3333ff)
         time.sleep(1)
         seconds += 1
         if seconds == 60:
