fef3e691770a15251cd1aaf334d4b019542ffedb
MTO <emteeoh@gmail.com>
2018-04-23 22:09:59 -0400

    converted font from dictionary to list of lists, and made a map to find the characters. also making .mpy files. and trying to structure the code to use as few arrays as possible.

---
+++ fontdef.py
@@ -0,0 +1,24 @@
+fontmap = '0123456789:'
+font = [
+    [True, True, True, True, True, True, True, True, True, False, False, False, False, False, False, True, True, False,
+     False, False, False, False, False, True, True, True, True, True, True, True, True, True],
+    [False, False, False, False, False, False, False, False, True, True, True, True, True, True, True, True, False,
+     False, False, False, False, False, False, False, False, False, False, False, False, False, False, False],
+    [False, True, False, False, False, True, True, True, True, False, False, True, False, False, False, True, True,
+     False, False, True, False, False, False, True, True, False, False, False, False, True, True, False],
+    [False, True, False, False, False, False, True, False, True, False, False, False, True, False, False, True, True,
+     False, False, True, False, False, False, True, False, True, True, True, False, True, True, False],
+    [True, True, True, True, False, False, False, False, False, False, False, False, True, False, False, False, False,
+     False, False, True, False, False, False, False, True, True, True, True, True, True, True, True],
+    [True, True, True, True, False, False, True, False, True, False, False, False, True, False, False, True, True,
+     False, False, True, False, False, False, True, False, True, True, True, False, False, False, True],
+    [False, True, True, True, False, True, True, False, True, False, False, True, False, False, False, True, True,
+     False, False, False, True, False, False, True, False, True, True, False, False, False, True, False],
+    [True, False, False, False, False, True, True, True, False, False, False, True, False, False, False, True, True,
+     False, False, True, False, False, False, False, False, False, False, False, False, True, True, True],
+    [False, True, True, True, False, True, True, False, True, False, False, True, False, False, False, True, True,
+     False, False, True, False, False, False, True, False, True, True, False, True, True, True, False],
+    [False, True, True, False, False, False, False, False, False, False, False, False, True, False, False, True, True,
+     False, False, True, False, False, False, False, True, True, True, True, True, True, True, False],
+    [False, False, True, False, False, True, False, False, False, False, False, False, False, False, False, False]
+]
---
+++ make.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+~/bin/mpy-cross Aclock.py
+~/bin/mpy-cross fontdef.py
+~/bin/mpy-cross myfont.py
+cp *.mpy /media/richard/CIRCUITPY
