17b0881343ea76bf32213eb15b3092843d248a28
MTO <emteeoh@gmail.com>
2018-04-25 14:05:59 -0400

    Switched from lists to tuples for the font. I hoped an immutable list, of sorts, might require less memory. Much shenanigans when I switched to CP3.0.0-aplha. I ended up rebuilding all the .mpy's and wiping the trinket filesystem to avoid files I overlooked...

--- make.sh
+++ make.sh
@@ -2,4 +2,9 @@
 ~/bin/mpy-cross Aclock.py
 ~/bin/mpy-cross fontdef.py
 ~/bin/mpy-cross rtc.py
+rm -rf /media/richard/CIRCUITPY/lib
+mkdir /media/richard/CIRCUITPY/lib
+cp lib-2.2.3/neopixel.mpy /media/richard/CIRCUITPY/lib
+cp -R lib-2.2.3/adafruit_bus_device /media/richard/CIRCUITPY/lib
+cp -R lib-2.2.3/adafruit_register /media/richard/CIRCUITPY/lib
 cp *.mpy /media/richard/CIRCUITPY
